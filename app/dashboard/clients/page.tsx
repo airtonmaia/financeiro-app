b653a1208ac7acb7e8c65827ab3201f9289a85b2
airtonmaia <airtonmaiamt@gmail.com>
2025-08-11 18:38:42 -0400

    Update page.tsx

--- app/dashboard/clients/page.tsx
+++ app/dashboard/clients/page.tsx
@@ -123,22 +123,22 @@ export default function ClientsPage() {
             {/* cadastro de clientes */}
           <Sheet>
       <SheetTrigger asChild>
-        <Button variant="outline">Open</Button>
+        <Button variant="outline">Cadastrar novo cliente</Button>
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
-          <SheetTitle>Edit profile</SheetTitle>
+          <SheetTitle>Adicionar novo Cliente</SheetTitle>
           <SheetDescription>
-            Make changes to your profile here. Click save when you&apos;re done.
+            Novo cliente
           </SheetDescription>
         </SheetHeader>
         <div className="grid flex-1 auto-rows-min gap-6 px-4">
           <div className="grid gap-3">
-            <Label htmlFor="sheet-demo-name">Name</Label>
+            <Label htmlFor="sheet-demo-name">Nome</Label>
             <Input id="sheet-demo-name" defaultValue="Pedro Duarte" />
           </div>
           <div className="grid gap-3">
-            <Label htmlFor="sheet-demo-username">Username</Label>
+            <Label htmlFor="sheet-demo-username">Usuário</Label>
             <Input id="sheet-demo-username" defaultValue="@peduarte" />
           </div>
         </div>
